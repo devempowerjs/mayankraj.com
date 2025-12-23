cbcf516686f37caf331f5cf28caddab14ba612e3
Mayank Raj (devempowerjs) <mayankrajpis2013@gmail.com>
2025-12-23 10:54:13 +0530

    Fix comment formatting in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { Projects } from "@/components/main/projects";
 import Skills from "@/components/main/skills";
 import { Achievements } from "@/components/main/achievements";
 
-// yaha pe ye comment nahi hatana mayank error throw kar raha h [ SELF NOTE ]
+// yaha pe ye comment nahi hatana mayank error throw kar raha h [SELF NOTE]
 export default function Home() {
   return (
     <main className="h-full w-full">
